d2fd3ad2545d597976d203c3ab0d268a10b63540
maarijaperic <mperic343@gmail.com>
2025-12-02 20:39:30 +0100

    Update GeneratedPluginRegistrant.swift

--- ZaMariju/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ZaMariju/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,15 +5,23 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
+import file_picker
+import firebase_core
 import flutter_inappwebview_macos
 import path_provider_foundation
+import purchases_flutter
 import share_plus
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
